a0a671d60f9f46843cdf055eb17edd4ae845efb6
Gengler Damien <gengler.damien@gmail.com>
2020-02-18 11:08:00 +0100

    Lab2 over

--- WPLab/WPLab/server.py
+++ WPLab/WPLab/server.py
@@ -17,11 +17,11 @@ def after_request(exception):
 
 
 #Decided to put POST request so the username and the password are not in the URL 
-@app.route('/sign_in', methods = ['POST'])
+@app.route('/sign_in', methods = ['GET'])
 def sign_in():
-    if request.method == 'POST' :
+    if request.method == 'GET' :
         data=request.get_json()
-        if 'username' in data and 'password' in data':
+        if 'username' in data and 'password' in data:
             username=data['username']
             if database_helper.check_user_exists_email(username):
                 if database_helper.get_password(username) == data['password'] :
@@ -32,7 +32,7 @@ def sign_in():
                     else:
                         database_helper.save_token(username,new_token)
                     answer = {"success" : "True", "message" : "Sucessfully signed in !" , "data": new_token }
-                    
+
                 else :
                     answer = {"success" : "False", "message" : "Wrong username or password" , "data": "" }
             else:
@@ -45,9 +45,9 @@ def sign_in():
         abort(404)
 
 #Tested and working 
-@app.route('/sign_up', methods = ['PUT'])
+@app.route('/sign_up', methods = ['POST'])
 def sign_up():
-    if request.method == 'PUT' :
+    if request.method == 'POST' :
         data = request.get_json()
         if 'username' in data and 'password' in data and 'firstName' in data and 'lastName' in data and 'gender' in data and 'city' in data and 'country' in data:
             username=data['username']
@@ -91,12 +91,11 @@ def sign_out():
         if 'token' in data :
             token = data['token']
             #print(database_helper.get_username_from_token(token))
-            if database_helper.get_username_from_token(token) is False:
+            if database_helper.check_user_logged_in_token(token) is False:
                 answer = {"success" : "False", "message" : "No such user logged in" , "data": "" }
             else: 
                 if database_helper.remove_token(token):
                     answer = {"success" : "True", "message" : "Sucessfully signed out !" , "data": "" }
-                    
                 else : 
                     answer = {"success" : "False", "message" : "Unable to sign out !" , "data": "" }
         else: 
@@ -111,21 +110,29 @@ def sign_out():
 @app.route('/change_password', methods = ['POST'])
 def change_password():
     if request.method == 'POST' :
-        token = request.form['token']
-        oldpwd = request.form['oldPassword']
-        newpwd = request.form['newPassword']
-        
-        if database_helper.get_password(database_helper.get_username_from_token(token))==oldpwd:
-            if len(newpwd) >=10 :
-                database_helper.change_password(token,newpwd)
-                answer = {"success" : "True", "message" : "Sucessfully changed password !" , "data": "" }
-                return json.dumps(answer), 200
-            else: 
-                answer = {"success" : "False", "message" : "New password is too short" , "data": "" }
-                return json.dumps(answer), 200
-        else:
-            answer = {"success" : "False", "message" : "Old passwords don't match" , "data": "" }
-            return json.dumps(answer), 200
+        data = request.get_json()
+        if 'token' in data and 'oldPassword' in data and 'newPassword' in data:
+            token = data['token']
+            oldpwd = data['oldPassword']
+            newpwd = data['newPassword']
+            username = database_helper.get_username_from_token(token)
+            print(username)
+            if username is not False :
+                if database_helper.get_password(username)==oldpwd:
+                    if len(newpwd) >=10 :
+                        if database_helper.change_password(token,newpwd) :
+                            answer = {"success" : "True", "message" : "Sucessfully changed password !" , "data": "" }
+                        else:
+                            answer = {"success" : "False", "message" : "Unable to change password" , "data": "" }
+                    else: 
+                        answer = {"success" : "False", "message" : "New password is too short" , "data": "" }
+                else:
+                    answer = {"success" : "False", "message" : "Old passwords don't match" , "data": "" }
+            else:
+                answer = {"success" : "False", "message" : "You are not logged in" , "data": "" }
+        else: 
+            answer = {"success" : "False", "message" : "Missing one or more field" , "data": "" }
+        return json.dumps(answer), 200
     else:
         abort(404)
 
@@ -133,15 +140,17 @@ def change_password():
 @app.route('/get_user_data_token', methods = ['POST'])
 def get_user_data_by_token():
     if request.method == 'POST' :
-        token = request.form['token']
-            
-        data = database_helper.get_user_data_from_token(token)
-        if data is False :
-            answer = {"success" : "False", "message" : "No such user logged in" , "data": "" }
-            return json.dumps(answer), 200
-        else : 
-            answer = {"success" : "True", "message" : "Here's the data" , "data": data }
-            return json.dumps(answer), 200
+        data = request.get_json()
+        if 'token' in data :
+            token = data['token']
+            data = database_helper.get_user_data_from_token(token)
+            if data is False :
+                answer = {"success" : "False", "message" : "No such user logged in" , "data": "" }
+            else : 
+                answer = {"success" : "True", "message" : "Retreiving data from server " , "data": data }
+        else :
+            answer = {"success" : "False", "message" : "Missing one or more field" , "data": "" }
+        return json.dumps(answer), 200
     else:
         abort(404)
 #Change to JSON 
